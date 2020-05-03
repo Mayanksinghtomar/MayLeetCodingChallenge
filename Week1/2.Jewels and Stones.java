31d3c796736a399408f71b40ac2c597775cf5ed7
Mayank Singh <mayank.mst18@gamil.com>
2020-05-03 14:54:25 +0530

    Week1

--- Week1/2.Jewels and Stones.java
+++ Week1/2.Jewels and Stones.java
@@ -1,4 +1,4 @@
-You're given strings J representing the types of stones that are jewels, and S representing the stones you have.  Each character in S is a type of stone you have.  You want to know how many of the stones you have are also jewels.
+/*You're given strings J representing the types of stones that are jewels, and S representing the stones you have.  Each character in S is a type of stone you have.  You want to know how many of the stones you have are also jewels.
 
 The letters in J are guaranteed distinct, and all characters in J and S are letters. Letters are case sensitive, so "a" is considered a different type of stone from "A".
 
@@ -12,7 +12,7 @@ Input: J = "z", S = "ZZ"
 Output: 0
 
 S and J will consist of letters and have length at most 50.
-The characters in J are distinct.
+The characters in J are distinct.*/
 
 class Solution {
     public int numJewelsInStones(String J, String S) {
@@ -27,5 +27,4 @@ class Solution {
         }
         return ans;
     }
-}
 }
